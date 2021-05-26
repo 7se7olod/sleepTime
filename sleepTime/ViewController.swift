b8dd21a69d8c6cb78892397f43f321869d376b9f
7se7olod <pushin_95@mail.ru>
2021-05-26 15:50:40 +0400

    ref

--- sleepTime/ViewController.swift
+++ sleepTime/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
     var oneLabel = ""
     let twoLabel = Date()
     let dateFormatter = DateFormatter()
-    //словари для хранения времени
-    var sleepArr = [1: "", 2: "", 3: "", 4: "", 5: "", 6: ""]
-    var sleepArray = [1: "", 2: "", 3: "", 4: "", 5: "", 6: ""]
+    
+    //массивы для хранения времени
+    var sleepArr: [String] = []
+    var sleepArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,65 +23,61 @@ class ViewController: UIViewController {
             calculateOutlet.isEnabled = false
         //смена цвета цифр в datePicker
             dataPickerOutlet.textColor = .white
-        
     }
     
-    //функция которая вычисляет время и добавляет в словарь
+    //функция которая вычисляет время и добавляет в массив
     func countingTime() {
         
-        let endTimeString = dateFormatter.string(from: twoLabel)
-        let endTime = dateFormatter.date(from: endTimeString)
         dateFormatter.timeStyle = DateFormatter.Style.short
 
-        sleepArray[6] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                           value: 540,
-                                                           endTime: endTime!))
-        sleepArray[5] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                           value: 450,
-                                                           endTime: endTime!))
-        sleepArray[4] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                           value: 360,
-                                                           endTime: endTime!))
-        sleepArray[3] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                           value: 270,
-                                                           endTime: endTime!))
-        sleepArray[2] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                           value: 180,
-                                                           endTime: endTime!))
-        sleepArray[1] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                           value: 90,
-                                                           endTime: endTime!))
+        sleepArray.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                             value: 540,
+                                                             endTime: twoLabel)), at: 0)
+        sleepArray.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                             value: 450,
+                                                             endTime: twoLabel)), at: 1)
+        sleepArray.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                            value: 360,
+                                                            endTime: twoLabel)), at: 2)
+        sleepArray.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                            value: 270,
+                                                            endTime: twoLabel)), at: 3)
+        sleepArray.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                            value: 180,
+                                                            endTime: twoLabel)), at: 4)
+        sleepArray.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                            value: 90,
+                                                            endTime: twoLabel)), at: 5)
                 
     }
     
     func countTime() {
         
         dateFormatter.timeStyle = DateFormatter.Style.short
-        let endTimeString = dateFormatter.date(from: oneLabel)
+        let endDateOne = dateFormatter.date(from: oneLabel )
         
-        sleepArr[1] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                         value: -540,
-                                                         endTime: endTimeString!))
-        sleepArr[2] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                         value: -450,
-                                                         endTime: endTimeString!))
-        sleepArr[3] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                         value: -360,
-                                                         endTime: endTimeString!))
-        sleepArr[4] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                         value: -270,
-                                                         endTime: endTimeString!))
-        sleepArr[5] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                         value: -180,
-                                                         endTime: endTimeString!))
-        sleepArr[6] = dateFormatter.string(from: getDate(byAdding: .minute,
-                                                         value: -90,
-                                                         endTime: endTimeString!))
+        sleepArr.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                           value: -540,
+                                                           endTime: endDateOne)), at: 0)
+        sleepArr.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                           value: -450,
+                                                           endTime: endDateOne!)), at: 1)
+        sleepArr.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                           value: -360,
+                                                           endTime: endDateOne!)), at: 2)
+        sleepArr.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                            value: -270,
+                                                            endTime: endDateOne!)), at: 3)
+        sleepArr.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                            value: -180,
+                                                            endTime: endDateOne!)), at: 4)
+        sleepArr.insert(dateFormatter.string(from: getDate(byAdding: .minute,
+                                                            value: -90,
+                                                            endTime: endDateOne!)), at: 5)
     }
     
-    func getDate(byAdding: Calendar.Component, value: Int, endTime: Date) -> Date! {
+    func getDate(byAdding: Calendar.Component, value: Int, endTime: Date!) -> Date! {
         let calendar = Calendar.current
-
         let endDate = calendar.date(byAdding: byAdding, value: value, to: endTime)
         return endDate
     }
@@ -93,24 +90,24 @@ class ViewController: UIViewController {
          guard let destination = segue.destination as? TimeViewController else { return }
         
             countTime()
-        destination.sleeper[1] = sleepArr[1]!
-        destination.sleeper[2] = sleepArr[2]!
-        destination.sleeper[3] = sleepArr[3]!
-        destination.sleeper[4] = sleepArr[4]!
-        destination.sleeper[5] = sleepArr[5]!
-        destination.sleeper[6] = sleepArr[6]!
-        destination.sleepNeed = "Ложитесь спать в это время:"
-        }
-        else if segue.identifier == "sleepSegue" {
+            destination.sleeper[1] = sleepArr[0]
+            destination.sleeper[2] = sleepArr[1]
+            destination.sleeper[3] = sleepArr[2]
+            destination.sleeper[4] = sleepArr[3]
+            destination.sleeper[5] = sleepArr[4]
+            destination.sleeper[6] = sleepArr[5]
+            destination.sleepNeed = "Ложитесь спать в это время:"
             
-        guard let destinationOne = segue.destination as? TimeViewController else { return }
+        } else if segue.identifier == "sleepSegue" {
+            
+         guard let destinationOne = segue.destination as? TimeViewController else { return }
             countingTime()
-            destinationOne.sleeper[1] = sleepArray[1]!
-            destinationOne.sleeper[2] = sleepArray[2]!
-            destinationOne.sleeper[3] = sleepArray[3]!
-            destinationOne.sleeper[4] = sleepArray[4]!
-            destinationOne.sleeper[5] = sleepArray[5]!
-            destinationOne.sleeper[6] = sleepArray[6]!
+            destinationOne.sleeper[1] = sleepArray[5]
+            destinationOne.sleeper[2] = sleepArray[4]
+            destinationOne.sleeper[3] = sleepArray[3]
+            destinationOne.sleeper[4] = sleepArray[2]
+            destinationOne.sleeper[5] = sleepArray[1]
+            destinationOne.sleeper[6] = sleepArray[0]
             destinationOne.sleepNeed = "Заведите будильник на это время:"
          
         } else {return}
@@ -133,6 +130,7 @@ class ViewController: UIViewController {
 
     @IBOutlet var calculateOutlet: UIButton!
 }
+
 
 //расширение для установки цвета datePicker
 
